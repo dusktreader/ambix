147b0efdfb07b78ddbd945fb9897cf8377c565b5
Tucker Beck <tucker.beck@rentrakmail.com>
2018-01-04 16:27:37 -0800

    Version bumped to v0.2.0

--- .project_metadata.py
+++ .project_metadata.py
@@ -1,11 +1,9 @@
-# This file is used to store metadata for the project and is consumed in
-# several places including setup.py and docs/conf.py
 {
-    'author':       'Tucker Beck',
+    'author': 'Tucker Beck',
     'author_email': 'tucker.beck@gmail.com',
-    'description':  'a tool for reordering alembic migrations',
-    'license':      'MIT',
-    'name':         'ambix',
-    'release':      '0.1.0',
-    'version':      '0.1',
+    'description': 'a tool for reordering alembic migrations',
+    'license': 'MIT',
+    'name': 'ambix',
+    'release': '0.2.0',
+    'version': '0.2',
 }
